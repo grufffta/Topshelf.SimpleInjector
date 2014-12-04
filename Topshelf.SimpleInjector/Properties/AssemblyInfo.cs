a1ee6fdb167d63d85f898ae1f1496b01e481772b
Gareth <gareth@distechsw.co.uk>
2014-12-04 12:47:56 +0000

    git version task

--- Topshelf.SimpleInjector/Properties/AssemblyInfo.cs
+++ Topshelf.SimpleInjector/Properties/AssemblyInfo.cs
@@ -31,7 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("0.2.0.0")]
-//[assembly: AssemblyFileVersion("0.2.0")]
-//[assembly: AssemblyInformationalVersion("0.2.0-unstable.1+1.Branch.develop.Sha.de25fa9db4f639ca5ddba74fc355257c5386c063")]
+
